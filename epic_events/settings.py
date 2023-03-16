53251a85ee5b22cd099b04403db5ed1c49cc29dc
bog <bog@gozmail.net>
2023-03-16 23:21:31 +0100

    ADD: now use pgsql.

--- epic_events/settings.py
+++ epic_events/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +81,10 @@ WSGI_APPLICATION = 'epic_events.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ocr12',
+        'USER': 'ocr12',
+        'PASSWORD': 'ocr12'
     }
 }
 
